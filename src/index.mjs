9a89b4004b797581934a51b73d33f578e19665ff
wulan17 <wulan17@nusantararom.org>
2023-06-11 11:21:37 +0700

    Mayuri changes
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- src/index.mjs
+++ src/index.mjs
@@ -1,19 +1,13 @@
-import explicitRenderHtml from './explicit.html';
-import implicitRenderHtml from './implicit.html';
-
-// This is the demo secret key. In prod, we recommend you store
-// your secret key(s) safely.
-const SECRET_KEY = '1x0000000000000000000000000000000AA';
-
-async function handlePost(request) {
+async function handlePost(request, env) {
     const body = await request.formData();
     // Turnstile injects a token in "cf-turnstile-response".
     const token = body.get('cf-turnstile-response');
     const ip = request.headers.get('CF-Connecting-IP');
+    const verify_id = body.get('verify_id');
 
     // Validate the token by calling the "/siteverify" API.
     let formData = new FormData();
-    formData.append('secret', SECRET_KEY);
+    formData.append('secret', env.SECRET_KEY);
     formData.append('response', token);
     formData.append('remoteip', ip);
 
@@ -23,27 +17,91 @@ async function handlePost(request) {
     });
 
     const outcome = await result.json();
-    if (!outcome.success) {
-        return new Response('The provided Turnstile token was not valid! \n' + JSON.stringify(outcome));
-    }
-    // The Turnstile token was successfuly validated. Proceed with your application logic.
-    // Validate login, redirect user, etc.
-    // For this demo, we just echo the "/siteverify" response:
-    return new Response('Turnstile token successfuly validated. \n' + JSON.stringify(outcome));
+    outcome['verify_id'] = verify_id;
+    const res = `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <style>
+            body {
+                --bg-color: var(--tg-theme-bg-color, #fff);
+                font-family: sans-serif;
+                background-color: var(--bg-color);
+                color: var(--tg-theme-text-color, #222);
+                font-size: 14px;
+                margin: 0;
+                padding: 0;
+                color-scheme: var(--tg-color-scheme);
+            }
+        </style>
+        <script src="https://telegram.org/js/telegram-web-app.js?1"></script>
+        <script>
+            function myFunction() {
+                var data = document.getElementById("result").value;
+                Telegram.WebApp.ready();
+                Telegram.WebApp.sendData(data);
+            }
+        </script>
+    </head>
+    <body onload="myFunction()">
+        <input type="hidden" id="result" value='`+JSON.stringify(outcome)+`' />
+    </body>
+</html>
+    `
+    return new Response(res, {
+        headers: {
+            'Content-Type': 'text/html',
+        },
+    });
 }
 
 export default {
-    async fetch(request) {
+    async fetch(request, env) {
         if (request.method === 'POST') {
-            return await handlePost(request);
+            return await handlePost(request, env);
         }
 
         const url = new URL(request.url);
-        let body = implicitRenderHtml;
-        if (url.pathname === '/explicit') {
-            body = explicitRenderHtml;
-        }
-        
+        let verify_id = url.searchParams.get('verify_id');
+        let body = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Turnstile &dash;</title>
+    <script src="https://challenges.cloudflare.com/turnstile/v0/api.js?onload=_turnstileCb" async defer></script>
+    <style>
+      body {
+        --bg-color: var(--tg-theme-bg-color, #fff);
+        font-family: sans-serif;
+        background-color: var(--bg-color);
+        color: var(--tg-theme-text-color, #222);
+        font-size: 14px;
+        margin: 0;
+        padding: 0;
+        color-scheme: var(--tg-color-scheme);
+        text-align: center;
+      }
+    </style>
+    <script src="https://telegram.org/js/telegram-web-app.js?1"></script>
+    <script>
+      function turnstile_callback () {
+        document.getElementById("form").submit();
+      }
+    </script>
+  </head>
+  <body>
+    <main class="form-signin">
+      <form method="POST" action="/" id="form">
+        <img src="https://images.emojiterra.com/google/noto-emoji/unicode-15/color/svg/1f916.svg" height="40px" width="40px" alt="robot" /><br />
+        <h1>Are you a human?</h1>
+        <i>Complete CAPTCHA below to join the group chat!</i>
+        <input type="hidden" name="verify_id" value="`+verify_id+`" />
+        <div class="cf-turnstile" data-sitekey="`+env.SITE_KEY+`" data-callback="turnstile_callback"></div>
+      </form>
+    </main>
+  </body>
+</html>
+        `;
         return new Response(body, {
             headers: {
                 'Content-Type': 'text/html',
